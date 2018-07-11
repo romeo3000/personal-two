4116def295e9da2163a991fd856fffbdae4cc291
romeo3000 <soccernumber10@gmail.com>
2018-07-11 02:51:07 -0600

    getting some css done to match UX/UI from Photoshop design!

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import  EventDashboard  from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <EventDashboard/>
+      </div>
+    );
+  }
+}
+
+export default App;
