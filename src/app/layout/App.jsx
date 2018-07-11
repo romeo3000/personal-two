2db66d40c33a6d3905ba597addcb7d9bfafbed48
romeo3000 <soccernumber10@gmail.com>
2018-07-11 03:05:13 -0600

    it's 3:04 am just add a container to fix left and right column.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Container } from 'semantic-ui-react'
 import  EventDashboard  from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container className="main">
         <NavBar/>
         <EventDashboard/>
-      </div>
+      </Container>
     );
   }
 }
