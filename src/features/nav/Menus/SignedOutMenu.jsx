4aade4547b8d2433d5f22943e30162f0f2c9a7ef
romeo3000 <soccernumber10@gmail.com>
2018-07-16 15:08:55 -0600

    Working on menu and signinmenu &SignOutMenu

---
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Menu, Button } from 'semantic-ui-react'
+
+const SignedOutMenu = () => {
+  return (
+    <Menu.Item position ="right">
+    <Button color='orange' content="Login" />
+    <Button color='yellow'content="Register" style={{marginLeft: '0.5em'}} /> 
+  </Menu.Item>
+  )
+}
+
+export default SignedOutMenu
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Menu, Container, Button, Image} from 'semantic-ui-react'
 import logo from '../../../assets//logo.png'
 import { NavLink, Link} from 'react-router-dom'
+import SignedOutMenu from '../Menus/SignedOutMenu'
+import SignedInMenu from '../Menus/SignedInMenu'
 
 
 export default class NavBar extends Component {
@@ -19,10 +21,9 @@ export default class NavBar extends Component {
            <Menu.Item>
              <Button as={Link} to='/createEvent' floated="right" positive inverted content="Create Game" />
            </Menu.Item>
-           <Menu.Item position ="right">
-             {/* <Button color='orange' content="Login" />
-             <Button color='yellow'content="Sign Out" style={{marginLeft: '0.5em'}} /> */}
-           </Menu.Item>
+           <SignedInMenu/>
+           <SignedOutMenu/>
+           
          </Container>
        </Menu>
 
