31a384c0d0ae75ee2797e0d03b929634c839c5e5
romeo3000 <soccernumber10@gmail.com>
2018-07-16 16:06:43 -0600

    working on settngs DashBoard

--- src/features/user/Settings/SettingDashBoard.jsx
+++ src/features/user/Settings/SettingDashBoard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Switch,Route } from 'react-router-dom'
-
 import { Grid } from 'semantic-ui-react'
 import SettingsNav from './SettingsNav'
+
 
 const SettingDashBoard = () => {
   return (
   <Grid>
     <Grid.Column width={12}>
+    <Switch>
+      <Route path='/settings/basics' component={BasicPage}/>
+      <Route path='/settings/about' component={AboutPage}/>
+      <Route path='/settings/photos' component={PhotosPage}/>
+      <Route path='/settings/account' component={AccountPage}/>
+    </Switch>
     <h1>Settings</h1>
     </Grid.Column>
     <Grid.Column width={12}>
