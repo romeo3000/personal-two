ba31bf72202b8b30f049d4814a1bd42a955bd08d
romeo3000 <soccernumber10@gmail.com>
2018-07-16 13:00:49 -0600

    Setting up Routes with React-router- dom

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { Container} from "semantic-ui-react";
+import { Route } from 'react-router-dom'
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import EventDetailed from "../../features/event/EventDetailed/EventDetailed";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
 
 
 class App extends Component {
@@ -10,7 +13,13 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
-          <EventDashboard className=""/>
+        <Route path='/' component={EventDashboard}/>
+        <Route path='/events' component={EventDashboard}/>
+        <Route path='/event/:id' component={EventDetailed}/>
+        <Route path='/people' component={PeopleDashboard}/>
+        <Route path='/profile/:id' component={UserDashboard}/>
+        <Route path='/settings' component={SettingsDashboard}/>
+        <Route path='/createEvent' component={EventForm}/>
          
         </Container>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +9,11 @@ import registerServiceWorker from './registerServiceWorker';
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render( <App />, rootEl)
+    ReactDOM.render(
+        <BrowserRouter>
+           <App />
+        </BrowserRouter>
+        , rootEl)
 }
     if (module.hot){
         module.hot.accept('./app/layout/App', () => {
