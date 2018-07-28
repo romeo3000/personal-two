2489f103f9afd8453e0afb2a81b9c0620de6cdf6
romeo3000 <soccernumber10@gmail.com>
2018-07-28 17:08:58 -0600

    semanti ui setup of account setup was complete not add componet page

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -15,7 +15,8 @@ const actions = {
 };
 
 const mapState = state => ({
-  auth: state.firebase.auth
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
 });
 
 class NavBar extends Component {
@@ -33,7 +34,7 @@ class NavBar extends Component {
     this.props.history.push('/');
   };
   render() {
-    const { auth } = this.props;
+    const { auth, profile } = this.props;
     const authenticated = auth.isLoaded && !auth.isEmpty;
 
     return (
@@ -61,7 +62,8 @@ class NavBar extends Component {
           }
           {authenticated ? (
             <SignedInMenu
-              auth={auth}
+            //i dont need auth any more {profile}
+              profile={profile}
               signOut={this.handleSignOut}
             />
           ) : (
